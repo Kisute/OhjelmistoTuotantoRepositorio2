4be43a06959ec920fcf3f4813544092ac821aa24
Kisute <jenhok@gmail.com>
2024-11-24 15:57:02 +0200

    1. Tutustuminen Robot Frameworkkiin

---
+++ osa2/poetry-web/oma_react_koodi.js
@@ -0,0 +1,9 @@
+/* jshint ignore:start */
+const root = ReactDOM.createRoot( document.getElementById('root'));
+root.render(
+      /* jshint ignore:start */
+  <h1>Hello, world!</h1>,
+      /* jshint ignore:end */
+);
+
+/* jshint ignore:end */
